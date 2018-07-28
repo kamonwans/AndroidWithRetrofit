d93c9ca7a2aa5f9f0a5f8c7f48241861236237a3
kamonwans <kamonwan294@gmail.com>
2018-07-28 14:00:49 +0700

    add variablev and create getter

--- app/src/main/java/com/example/liuyao/androidwithretrofit/Users.java
+++ app/src/main/java/com/example/liuyao/androidwithretrofit/Users.java
@@ -4,10 +4,34 @@ import com.google.gson.annotations.SerializedName;
 
 class Users {
     private String login;
-
-    @SerializedName("avatar_url")
     private String avartaUrl;
     private String type;
+    private String name;
+    private String company;
+    private String blog;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getBlog() {
+        return blog;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    private String bio;
 
     public String getLogin() {
         return login;
