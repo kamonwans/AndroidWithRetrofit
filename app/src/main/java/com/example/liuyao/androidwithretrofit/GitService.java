7d65bda2d5e17261b3ca319664e45a6bc8196c77
kamonwans <kamonwan294@gmail.com>
2018-07-29 13:41:38 +0700

    change part to path

--- app/src/main/java/com/example/liuyao/androidwithretrofit/GitService.java
+++ app/src/main/java/com/example/liuyao/androidwithretrofit/GitService.java
@@ -5,12 +5,13 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 public interface GitService {
     @GET("users")
     Call<List<Users>> getListUsers();
 
     @GET("users/{login}")
-    Call<Users> getUserDetail(@Part("login") String login);
+    Call<Users> getUserDetail(@Path("login") String login);
 
 }
