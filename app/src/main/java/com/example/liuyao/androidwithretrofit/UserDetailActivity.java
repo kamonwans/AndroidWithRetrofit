0323e788a456f806cb9439b32937901b387b8822
kamonwans <kamonwan294@gmail.com>
2018-07-29 12:43:03 +0700

    bindBiew with id

--- app/src/main/java/com/example/liuyao/androidwithretrofit/UserDetailActivity.java
+++ app/src/main/java/com/example/liuyao/androidwithretrofit/UserDetailActivity.java
@@ -2,12 +2,28 @@ package com.example.liuyao.androidwithretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UserDetailActivity extends AppCompatActivity {
+    private ImageView imgAvatar;
+    private TextView tvName;
+    private TextView tvCompany;
+    private TextView tvBlog;
+    private TextView tvBio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
+        initInstance();
+    }
+
+    private void initInstance() {
+        imgAvatar = findViewById(R.id.imgAvarta);
+        tvName = findViewById(R.id.tvName);
+        tvCompany = findViewById(R.id.tvComapy);
+        tvBlog = findViewById(R.id.tvBlog);
+        tvBio = findViewById(R.id.tvBio);
     }
 }
