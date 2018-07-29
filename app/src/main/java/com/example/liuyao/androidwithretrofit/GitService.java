a35bf31b47ff7348efdd6e32a269361f1dea544b
kamonwans <kamonwan294@gmail.com>
2018-07-29 13:05:02 +0700

    add new api

--- app/src/main/java/com/example/liuyao/androidwithretrofit/GitService.java
+++ app/src/main/java/com/example/liuyao/androidwithretrofit/GitService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Part;
 
 public interface GitService {
-    @GET("/users")
+    @GET("users")
     Call<List<Users>> getListUsers();
+
+    @GET("users/{user}")
+    Call<Users> getUserDetail(@Part("user") String login);
 
 }
