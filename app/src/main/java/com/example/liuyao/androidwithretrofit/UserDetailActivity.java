7253be413f9f66d029cafc617a6f4ef19dcd1d73
kamonwans <kamonwan294@gmail.com>
2018-07-29 13:44:30 +0700

    call service and set data to view

--- app/src/main/java/com/example/liuyao/androidwithretrofit/UserDetailActivity.java
+++ app/src/main/java/com/example/liuyao/androidwithretrofit/UserDetailActivity.java
@@ -6,12 +6,19 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class UserDetailActivity extends AppCompatActivity {
     private ImageView imgAvatar;
     private TextView tvName;
     private TextView tvCompany;
     private TextView tvBlog;
     private TextView tvBio;
+    private Users users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,7 @@ public class UserDetailActivity extends AppCompatActivity {
     }
 
     private void initInstance() {
-        imgAvatar = findViewById(R.id.imgAvarta);
+        imgAvatar = findViewById(R.id.imgAvatar);
         tvName = findViewById(R.id.tvName);
         tvCompany = findViewById(R.id.tvComapy);
         tvBlog = findViewById(R.id.tvBlog);
@@ -29,5 +36,26 @@ public class UserDetailActivity extends AppCompatActivity {
 
       String login =  getIntent().getExtras().getString("login");
 
+      final Call<Users> user = RetrofitClient.getInstance().getGitService().getUserDetail(login);
+      user.enqueue(new Callback<Users>() {
+          @Override
+          public void onResponse(Call<Users> call, Response<Users> response) {
+              if (response.isSuccessful()){
+                  if (response.body() !=  null){
+                    users = response.body();
+                    tvName.setText(users.getName());
+                    tvCompany.setText(users.getCompany());
+                    tvBlog.setText(users.getBlog());
+                    tvBio.setText(users.getBio());
+                    Glide.with(UserDetailActivity.this).load(users.getAvartaUrl()).into(imgAvatar);
+                  }
+              }
+          }
+
+          @Override
+          public void onFailure(Call<Users> call, Throwable t) {
+
+          }
+      });
     }
 }
