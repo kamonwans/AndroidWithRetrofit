e45737cd94e76e0a475cfd552192986dcedc2993
kamonwans <kamonwan294@gmail.com>
2018-07-29 13:17:43 +0700

    implement paracelable

--- app/src/main/java/com/example/liuyao/androidwithretrofit/Users.java
+++ app/src/main/java/com/example/liuyao/androidwithretrofit/Users.java
@@ -1,8 +1,11 @@
 package com.example.liuyao.androidwithretrofit;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-class Users {
+class Users implements Parcelable{
     private String login;
     private String avartaUrl;
     private String type;
@@ -10,6 +13,46 @@ class Users {
     private String company;
     private String blog;
     private String email;
+
+    protected Users(Parcel in) {
+        login = in.readString();
+        avartaUrl = in.readString();
+        type = in.readString();
+        name = in.readString();
+        company = in.readString();
+        blog = in.readString();
+        email = in.readString();
+        bio = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(login);
+        dest.writeString(avartaUrl);
+        dest.writeString(type);
+        dest.writeString(name);
+        dest.writeString(company);
+        dest.writeString(blog);
+        dest.writeString(email);
+        dest.writeString(bio);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Users> CREATOR = new Creator<Users>() {
+        @Override
+        public Users createFromParcel(Parcel in) {
+            return new Users(in);
+        }
+
+        @Override
+        public Users[] newArray(int size) {
+            return new Users[size];
+        }
+    };
 
     public String getName() {
         return name;
